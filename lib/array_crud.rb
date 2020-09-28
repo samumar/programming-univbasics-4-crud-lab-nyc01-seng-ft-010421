67544680e04f381c7e794b0c66f27b57f3268ce0
samumar43 <samumar43@gmail.com>
2020-09-28 03:32:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [element0, element1, element2, element3, element4]
+  array = [0, 1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
