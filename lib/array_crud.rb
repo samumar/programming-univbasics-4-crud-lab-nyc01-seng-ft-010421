1c0c546bc89e140268b1f705a991f7eadb1969e2
samumar43 <samumar43@gmail.com>
2020-09-28 03:37:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,6 +38,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-index_number = "element"
-array[index_number] = element
+index_number = element
+array[index_number] = "element"
 end
